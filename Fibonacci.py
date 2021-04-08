62933877e204ac7878b3864db7407b88d27238c6
GbotemiB <gbotemibolarinwa@gmail.com>
2021-04-08 00:05:52 +0000

    cache added

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,10 +3,13 @@ cache = {}
 def fib(n):
     if n in cache:
         return cache[n]
+
     if (n == 0):
         return 0
+
     if (n == 1):
         return 1
+        
     val = (fib(n - 1) + fib(n - 2))
     cache[n] = val
     return val
